e591801a4ddd6901dcd5db61d7ff5b3c6e33ac2d
wentianlou <429522316@qq.com>
2019-12-02 19:40:25 +0800

    '2'

---
+++ src/main/java/com/study/javastudy/JavaStudyApplication.java
@@ -0,0 +1,14 @@
+package com.study.javastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStudyApplication.class, args);
+        System.out.println(".....service start ...");
+    }
+
+}
---
+++ src/test/java/com/study/javastudy/JavaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.javastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
